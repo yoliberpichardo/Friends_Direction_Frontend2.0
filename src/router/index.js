ab1826d531eff8cde5200dc655b5e2f02f4842cc
Yoliber Pichardo <yoliberfelixpp01@gmail.com>
2022-07-20 22:02:33 -0400

    .../

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { route } from 'quasar/wrappers'
 import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
 import routes from './routes'
 
+const token = localStorage.getItem('token')
+
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
@@ -15,6 +17,16 @@ export default route(function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE)
+  })
+
+  Router.beforeEach(async (to, from) => {
+    if (
+      // make sure the user is authenticated
+      !token && to.name !== 'Register' && to.name !== 'Login'
+    ) {
+      // redirect the user to the login page
+      return { name: 'Login' }
+    }
   })
 
   return Router
